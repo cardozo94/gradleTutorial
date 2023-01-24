557bb3beb44a93874f6ecaaebc43749ff14db302
Cristian Cardozo <cristian.cardozo@ticketmaster.com>
2023-01-24 15:13:33 -0500

    Initial commit.

---
+++ src/main/net/camcar/gradleTutorial/Main.java
@@ -0,0 +1,10 @@
+package net.camcar.gradleTutorial;
+
+import java.util.Arrays;
+import org.apache.commons.lang3.StringUtils;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println(StringUtils.swapCase(Arrays.toString(args)));
+    }
+}
